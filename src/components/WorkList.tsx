9529f7075e8a3707443d29ca4e0c39d147b7173c
satoshi <fuji02001458@gmail.com>
2024-03-18 23:44:45 +0900

    add book-stock-app

--- src/components/WorkList.tsx
+++ src/components/WorkList.tsx
@@ -8,23 +8,28 @@ import { FadeInBottom } from "./FadeInBottom";
 import RecorderApp1 from "../../public/articleRecorder1.gif";
 import RecorderApp2 from "../../public/articleRecorder2.gif";
 import RecorderApp3 from "../../public/articleRecorder3.gif";
+import BookStockApp1 from "../../public/book-stock-app1.gif";
+import { Card } from "./ui/card";
 
 export default function WorkList() {
   return (
     <FadeInBottom>
       <div className="mx-10 my-10" id="portfolio">
         <h2 className="text-xl m-2 font-bold">制作実績</h2>
-        <p className="m-2">
-          使用したライブラリ、フレームワーク:React,Next.js,TailwindCSS,shadcn-UI
+        <p className="m-2w-full ">
+          使用したライブラリ、フレームワーク:
+          <br />
+          React,Next.js,TailwindCSS,shadcn-UI
         </p>
-        <div className="m-2 grid grid-cols-1 md:grid-cols-2 md:gap-x-10">
+
+        <div className="m-2 grid grid-cols-1 md:grid-cols-2 md:gap-x-10 md:gap-y-4">
           {/*app1*/}
-          <div className="p-2 w-full flex flex-col gap-4 mt-2">
+          <Card className="p-2 w-full flex flex-col gap-4 mt-2">
             <h3 className="font-title font-bold text-xl">My Shopping App</h3>
             <div className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
                 <p>
-                  買い物情報を管理するとともに、実績をデータベースへ格納し、各種チャートとして表示
+                  買い物情報をTODOリスト的に管理するとともに、金額や個数などの実績をデータベースへ格納し、各種チャートとして表示
                 </p>
                 <p className="text-xs">（制作期間：土日を中心に１週間程度）</p>
                 <div className="flex gap-x-4">
@@ -35,24 +40,17 @@ export default function WorkList() {
                     DEMO
                     <ArrowUpRightIcon className="w-6" />
                   </a>
-                  {/*<a
-                    href="https://github.com/satoshi-fujimori/202402-shopping-app"
-                    className="font-title flex"
-                  >
-                    SOURCE
-                    <ArrowUpRightIcon className="w-6" />
-  </a>*/}
                 </div>
               </div>
               <div className="flex gap-x-4 overflow-x-auto">
-                <Image src={ShoppingApp1} alt="app1" />
-                <Image src={ShoppingApp2} alt="app2" />
+                <Image src={ShoppingApp1} alt="app1" width={300} />
+                <Image src={ShoppingApp2} alt="app2" width={300} />
               </div>
             </div>
-          </div>
+          </Card>
 
           {/*app2*/}
-          <div className="p-2 w-full flex flex-col gap-4 mt-2">
+          <Card className="p-2 w-full flex flex-col gap-4 mt-2">
             <h3 className="font-title font-bold text-xl">Order App</h3>
             <div className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
@@ -68,24 +66,17 @@ export default function WorkList() {
                     DEMO
                     <ArrowUpRightIcon className="w-6" />
                   </a>
-                  {/*<a
-                    href="https://github.com/satoshi-fujimori/order-app"
-                    className="font-title flex"
-                  >
-                    SOURCE
-                    <ArrowUpRightIcon className="w-6" />
-  </a>*/}
                 </div>
               </div>
             </div>
             <div className="flex gap-x-4 overflow-x-auto">
-              <Image src={OrderApp1} alt="app1" />
-              <Image src={OrderApp2} alt="app2" />
+              <Image src={OrderApp1} alt="app1" width={300} />
+              <Image src={OrderApp2} alt="app2" width={300} />
             </div>
-          </div>
+          </Card>
 
           {/*app3*/}
-          <div className="p-2 w-full flex flex-col gap-4 mt-2">
+          <Card className="p-2 w-full flex flex-col gap-4 mt-2">
             <h3 className="font-title font-bold text-xl">
               My Article Record App
             </h3>
@@ -95,24 +86,30 @@ export default function WorkList() {
                   QiitaAPIで記事を取得し、読んだ記録をデータベースに格納し管理。またQiitaの過去の各月のタグ別の記事数をチャート表示
                 </p>
                 <p className="text-xs">（制作期間：土日を中心に３日程度）</p>
-                {/*<div className="flex gap-x-4">
-                  <a href="" className="font-title flex">
-                    DEMO
-                    <ArrowUpRightIcon className="w-6" />
-                  </a>
-                  <a href="" className="font-title flex">
-                    SOURCE
-                    <ArrowUpRightIcon className="w-6" />
-</a>*
-</div>*/}
+              </div>
+            </div>
+            <div className="flex gap-x-4 overflow-x-auto md:overflow-x-scroll">
+              <Image src={RecorderApp1} alt="app1" width={300} />
+              <Image src={RecorderApp2} alt="app1" width={300} />
+              <Image src={RecorderApp3} alt="app1" width={300} />
+            </div>
+          </Card>
+
+          {/*app4*/}
+          <Card className="p-2 w-full flex flex-col gap-4 mt-2">
+            <h3 className="font-title font-bold text-xl">Book Stock App</h3>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <p>
+                  GoogleBooksAPIで書籍情報を取得・表示し、カーリルAPIで現在地などから近い順に図書館情報及びその書籍の在庫状況を取得し、GoogleMapとテーブルで表示
+                </p>
+                <p className="text-xs">（制作期間：土日を中心に１日程度）</p>
               </div>
             </div>
             <div className="flex gap-x-4 overflow-x-auto md:overflow-visible">
-              <Image src={RecorderApp1} alt="app1" />
-              <Image src={RecorderApp2} alt="app1" />
-              <Image src={RecorderApp3} alt="app1" />
+              <Image src={BookStockApp1} alt="app1" width={300} />
             </div>
-          </div>
+          </Card>
         </div>
       </div>
     </FadeInBottom>
